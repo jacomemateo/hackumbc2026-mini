9965e92342b513df7e1223b6b7eb843a88fc85e5
Mateo Jacome <j300@umbc.edu>
2026-04-18 14:51:32 -0400

    feat: add CreateCourse functionality and update CourseHandler routes

--- server/internal/transport/http/dto/courses_dto.go
+++ server/internal/transport/http/dto/courses_dto.go
@@ -1,8 +1,14 @@
 package dto
 
 type CourseResponse struct {
-	ID              string 	  `json:"id"`
-	CourseName      string    `json:"course_name"`
-	CourseID        string    `json:"course_id"`
-	ProfessorName   string    `json:"professor_name"`
+	ID            string `json:"id"`
+	CourseName    string `json:"course_name"`
+	CourseID      string `json:"course_id"`
+	ProfessorName string `json:"professor_name"`
+}
+
+type CreateCourseRequest struct {
+	CourseName    string `json:"course_name" validate:"required"`
+	CourseID      string `json:"course_id" validate:"required"`
+	ProfessorName string `json:"professor_name" validate:"required"`
 }
