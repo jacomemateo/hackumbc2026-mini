b1def5e737f31cebc655cf6b9e253e9e8c6f5ca6
Mateo Jacome <j300@umbc.edu>
2026-04-18 11:19:15 -0400

    add validation package with struct validation functionality

---
+++ server/internal/validation/validation.go
@@ -0,0 +1,9 @@
+package validation
+
+import "github.com/go-playground/validator/v10"
+
+var validate = validator.New(validator.WithRequiredStructEnabled())
+
+func Struct(v any) error {
+	return validate.Struct(v)
+}
